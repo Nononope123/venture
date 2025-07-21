d625696a56f014de08faa3fb5eb62f9970164a63
Nononope123 <aureliamendame1@gmail.com>
2025-07-21 16:34:20 +0100

    ajout des heros

--- app/services/page.js
+++ app/services/page.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Avis from "../components/avis"
 import Coaching from "../components/coaching"
 import Formation from "../components/formation"
+import Heroavis from "../components/hero-avis"
 
 
 
 function page() {
   return (
     <div>
+      <Heroavis/>
       <div className="">
         <Coaching/>
         <Formation/>
